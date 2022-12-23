e81b4b751cf0a38ebe1bb20223a5999eb6b384ef
Nuno Caneco <nuno.caneco@gmail.com>
2022-12-23 00:45:47 +0000

    Station config with properties

--- SICCA.Web.Spike/Pages/Admin/StationConfigBase.cs
+++ SICCA.Web.Spike/Pages/Admin/StationConfigBase.cs
@@ -9,14 +9,24 @@ public class StationConfigBase : ComponentBase
     [Inject]
     public StationService StationService { get; set; }
 
+    [Parameter]
+    public string StationId { get; set; } = string.Empty;
+
     protected IEnumerable<Station> Stations { get; set; } = Enumerable.Empty<Station>();
 
-    protected Station SelectedStation { get; set; }
+    protected Station? SelectedStation { get; set; }
 
     protected override Task OnInitializedAsync()
     {
-        this.Stations = StationService.GetStations();
+        this.Stations = StationService.GetStations().OrderBy(s => s.Name);
+
+        if (!string.IsNullOrEmpty(StationId) && int.TryParse(StationId, out var stationId))
+        {
+            SelectedStation = Stations.SingleOrDefault(s => s.Id == stationId);
+        }
 
         return base.OnInitializedAsync();
     }
+
+    //public void SelectStation(Station station) => this.SelectedStation = station;
 }
--- SICCA.Web.Spike/Services/StationService.cs
+++ SICCA.Web.Spike/Services/StationService.cs
@@ -6,7 +6,26 @@ public class StationService
 {
     public IEnumerable<Station> GetStations()
     {
-        yield return new Station(625, "Campolide");
-        yield return new Station(1025, "Monte Abrãao");
+        yield return new Station(10 , " Monte Abraão");
+        yield return new Station(59 , " Mercês");
+        yield return new Station(121, " Rio de Mouro");
+        yield return new Station(224, " Queluz Belas");
+        yield return new Station(260, " Rossio");
+        yield return new Station(356, " Amadora");
+        yield return new Station(429, " Oeiras");
+        yield return new Station(469, " Cascais");
+        yield return new Station(525, " Cais do Sodré");
+        yield return new Station(621, " Campolide");
+        yield return new Station(656, " Benfica");
+        yield return new Station(732, " Santa Cruz - Damaia");
+        yield return new Station(775, " Reboleira");
+        yield return new Station(838, " Algueirão -Mem Martins");
+        yield return new Station(897, " Portela de Sintra");
+        yield return new Station(957, " Sintra");
+        yield return new Station(988, " Algés");
+        yield return new Station(105, " Massamá -Barcarena");
+        yield return new Station(113, " Agualva -Cacém");
+        yield return new Station(122, " Carcavelos");
+        yield return new Station(131, " Meleças");
     }
 }
