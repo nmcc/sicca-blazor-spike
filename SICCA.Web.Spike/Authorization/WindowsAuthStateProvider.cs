88a4ad45ba21ba269cb4e4ef5153eac69b6cd26b
Nuno Caneco <nuno.caneco@gmail.com>
2022-12-29 20:52:07 +0000

    Render menu options depending on role

--- SICCA.Web.Spike/Authorization/WindowsAuthStateProvider.cs
+++ SICCA.Web.Spike/Authorization/WindowsAuthStateProvider.cs
@@ -13,10 +13,10 @@ public class WindowsAuthStateProvider : AuthenticationStateProvider
         var identity = new ClaimsIdentity(new[]
             {
                 new Claim(ClaimTypes.Name, WindowsIdentity.GetCurrent().Name),
-                new Claim(ClaimTypes.Role, "Administrador")
+                new Claim(ClaimTypes.Role, "Administrator"),
+                new Claim(ClaimTypes.Role, "Operador Estação")
             }, "Windows");
 #pragma warning restore CA1416 // Validate platform compatibility
-
 
         var user = new ClaimsPrincipal(identity);
         return await Task.FromResult(new AuthenticationState(user));
