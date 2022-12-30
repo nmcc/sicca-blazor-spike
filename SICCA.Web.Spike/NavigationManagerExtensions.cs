82f65f6f042b5798c822ba1408d8fcba0c415e0a
Nuno Caneco <nuno.caneco@gmail.com>
2022-12-30 15:17:52 +0000

    Support different layout for monitoring urls

---
+++ SICCA.Web.Spike/NavigationManagerExtensions.cs
@@ -0,0 +1,7 @@
+﻿namespace Microsoft.AspNetCore.Components;
+
+public static class NavigationManagerExtensions
+{
+    public static string GetRelativeUri(this NavigationManager navigationManager) 
+        => $"/{navigationManager.ToBaseRelativePath(navigationManager.Uri)}";
+}
