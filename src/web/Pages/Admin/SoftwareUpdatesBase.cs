c286c865ef43e6ce5553fdc65b545be320ad0cd4
Nuno Caneco <nuno.caneco@gmail.com>
2023-01-16 08:43:53 +0000

    Add sample table on Software Updates

---
+++ src/web/Pages/Admin/SoftwareUpdatesBase.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace SICCA.Web.Spike.Pages.Admin;
+
+public class SoftwareUpdatesBase : ComponentBase
+{
+    protected IList<SoftwareUpdateEntry> Entries = new List<SoftwareUpdateEntry>();
+
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+
+        for (int i = 0; i < 10; ++i)
+        {
+            Entries.Add(new SoftwareUpdateEntry(i, $"Ficheiro {i}", $"SV{i}.zip", $"v{i}", null, $"/SV/{i}.zip"));
+        }
+    }
+}
+
+public record SoftwareUpdateEntry(
+    int Id,
+    string Description,
+    string FileName,
+    string Version,
+    DateTime? StartDateTime,
+    string FileUrl
+    );
